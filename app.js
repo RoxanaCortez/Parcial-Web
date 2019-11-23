61eacd1e659aa752359e7d1e7579baab23e60558
RoxanaCortez <00502211@uca.edu.sv>
2019-11-23 08:27:54 -0600

    add: agregando la conexion de la base conecta :)

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const config = require("./bin/config");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var obraRouter = require('./routes/obra');
 
 var app = express();
+config.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/obra', obraRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
